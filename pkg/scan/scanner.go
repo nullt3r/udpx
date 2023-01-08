1a86892332335aa4cd114d16358d9ab4c5c67c1c
nullt3r <nullt3r@bugdelivery.com>
2023-01-08 21:59:34 +0100

    fixed bug while displaying binary data in json output

--- pkg/scan/scanner.go
+++ pkg/scan/scanner.go
@@ -85,7 +85,7 @@ func (s Scanner) Run() {
 									log.Printf("[+] Received packet: %s%s%s...", colors.SetColor().Yellow, hex.EncodeToString(recv_Data), colors.SetColor().Reset)
 								}
 								
-								s.Result <- fmt.Sprintf(`{"address": "%s", "hostname": "%s", "protocol": "udp", "portid": "%d", "port_state": "open", "service_name": "%s", "service_product": null, "service_version": null, "extrainfo": "%s"}`, ip, domain, port, probe.Name, utils.DataToReadableFormat(recv_Data))
+								s.Result <- fmt.Sprintf(`{"address": "%s", "hostname": "%s", "protocol": "udp", "portid": "%d", "port_state": "open", "service_name": "%s", "service_product": null, "service_version": null, "extrainfo": "%s"}`, ip, domain, port, probe.Name, utils.EscapeByteArray(recv_Data))
 								return
 							}
 						}
@@ -140,7 +140,7 @@ func (s Scanner) Run() {
 							if s.Arg_sp {
 								log.Printf("[+] Received packet: %s%s%s...", colors.SetColor().Yellow, hex.EncodeToString(recv_Data), colors.SetColor().Reset)
 							}
-							s.Result <- fmt.Sprintf(`{"address": "%s", "hostname": null, "protocol": "udp", "portid": %d, "port_state": "open", "service_name": "%s", "service_product": null, "service_version": null, "extrainfo": "%s"}`, ip, port, probe.Name, utils.DataToReadableFormat(recv_Data))
+							s.Result <- fmt.Sprintf(`{"address": "%s", "hostname": null, "protocol": "udp", "portid": %d, "port_state": "open", "service_name": "%s", "service_product": null, "service_version": null, "extrainfo": "%s"}`, ip, port, probe.Name, utils.EscapeByteArray(recv_Data))
 							return
 						}
 					}
